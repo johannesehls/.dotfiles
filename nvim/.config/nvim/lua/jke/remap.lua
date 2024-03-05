0105dae0c96c27f661baee0869ba0c09a0cf489f
johannesehls <johannes.ehls@web.de>
2024-03-05 21:59:21 +0100

    add minor refactorings

--- nvim/.config/nvim/lua/jke/remap.lua
+++ nvim/.config/nvim/lua/jke/remap.lua
@@ -35,7 +35,7 @@ vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
--- quickfix list ...
+-- quickfix list / locations list ...
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
